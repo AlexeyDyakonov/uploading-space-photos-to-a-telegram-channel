09ec9b65c2e5cc4fe67e4eec9db9def6b864d2fa
Alexey Dyakonov <101695592+AlexeyDyakonov@users.noreply.github.com>
2023-03-31 23:34:56 +0500

    Update nasa_apod_images.py

--- nasa_apod_images.py
+++ nasa_apod_images.py
@@ -7,15 +7,6 @@ from dotenv import load_dotenv
 from load_photo_from_internet import load_photo_from_internet
 
 
-def fetch_spacex_last_launch(spacex_url):
-    response = requests.get(spacex_url)
-    response.raise_for_status()
-    pictures_url = response.json()['links']['flickr']['original']
-    for picture_index, picture_url in enumerate(pictures_url, start=1):
-        load_photo_from_internet(picture_url, f'images/'
-                                              f'spacex{picture_index}.jpg')
-
-
 if __name__ == '__main_':
     parser = argparse.ArgumentParser(
         description='Программа загружает фото дня по версии NASA'
